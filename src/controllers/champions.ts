4d926d4801219f4071eae950ff3a5e5508999d28
Lisec, George <glisec@iu.edu>
2026-03-04 15:04:06 -0500

    finished ban rate endpoint

--- src/controllers/champions.ts
+++ src/controllers/champions.ts
@@ -1,15 +1,28 @@
 import type { Request, Response } from "express";
+import { championBanRate } from "../services/hexcore/champions.js";
+import { success } from "zod/mini";
 
 export const getChampionBanRate = async (res: Response, req: Request) => {
   try {
     const champion = req.params.champion as string;
 
-    if (champion) {
+    if (!champion) {
       return res.status(400).json({
         success: false,
-        message: error instanceof Error ? error.message : "Unkown Error",
+        message: "Champion input required.",
       });
     }
+
+    const data = championBanRate(champion);
+
+    if (!data) {
+      return res.status(400).json({
+        success: false,
+        message: "Missing ban rate",
+      });
+    }
+
+    return res.status(200).json({ success: true, data: data });
   } catch (error) {
     return res.status(400).json({
       success: false,
