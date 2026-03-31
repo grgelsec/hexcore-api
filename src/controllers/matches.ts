b8371e05acefaec2a99c3a9f01323a23127a18b2
Lisec, George <glisec@iu.edu>
2026-03-31 11:57:36 -0400

    cache aside moved to reids folder

--- src/controllers/matches.ts
+++ src/controllers/matches.ts
@@ -2,8 +2,7 @@ import { BadRequestError, NotFoundError } from "@error";
 import { returnRecentMatches } from "@services/hexcore";
 import type { RiotParticipantDto } from "@types";
 import type { Request, Response } from "express";
-import { redis } from "@redis";
-import { getParamHashKey } from "@utils";
+import { cacheAside } from "../db/redis/cache-aside.js";
 
 export const getRecentMatches = async (req: Request, res: Response) => {
   const riotId = req.params.riotId as string;
@@ -11,36 +10,10 @@ export const getRecentMatches = async (req: Request, res: Response) => {
 
   if (!riotId) throw new BadRequestError("Riot ID is required");
 
-  const result = await cacheAside(returnRecentMatches, riotId);
+  const result = await cacheAside<RiotParticipantDto[]>(
+    returnRecentMatches,
+    riotId,
+  );
 
   return res.json(result);
 };
-
-async function cacheAside<T>(
-  fn: (requestParam: string) => Promise<T>,
-  requestParam: string,
-) {
-  let res: { data: T | null; isFromCache: boolean } = {
-    data: null,
-    isFromCache: false,
-  };
-
-  const hashkey = getParamHashKey(requestParam);
-  const cachedData = await redis.get(hashkey);
-  const docArr = cachedData ? JSON.parse(cachedData) : [];
-
-  if (docArr && docArr.length) {
-    res.data = docArr;
-    res.isFromCache = true;
-  } else {
-    const result: T = await fn(requestParam);
-
-    if (result) {
-      redis.set(hashkey, JSON.stringify(result), "EX", 60);
-    }
-
-    res.data = result;
-  }
-
-  return res;
-}
